25ec03a7361988f4b1a07cc9d874f494829ad254
Shankar Kumar <shankar@spotsoon.com>
2017-09-26 16:36:03 +0530

    add all the files to git

---
+++ app/src/main/java/apextechies/etodo/adapter/CustomPagerAdapter.java
@@ -0,0 +1,140 @@
+package apextechies.etodo.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import apextechies.etodo.R;
+import apextechies.etodo.model.BannerImageModel;
+
+public class CustomPagerAdapter extends PagerAdapter {
+
+	Context mContext;
+	Activity activity;
+	LayoutInflater mLayoutInflater;
+	ArrayList<BannerImageModel> models;
+	Timer timer;
+	int page = 1;
+	private boolean isDynamic = false;
+	private ViewPager viewPager;
+
+	@Override
+	public int getCount() {
+		return models.size();
+	}
+
+	public void setData(ArrayList<BannerImageModel> res, Context context, Activity activity) {
+		this.models = res;
+		this.mContext = context;
+		this.activity = activity;
+		mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	public void isDynamic(boolean isAnimatable) {
+		this.isDynamic = isAnimatable;
+	}
+
+	public void setVP(ViewPager viewPager) {
+		this.viewPager = viewPager;
+	}
+
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return view == object;
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, final int position) {
+		View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+
+		final ImageView imageView = (ImageView) itemView.findViewById(R.id.img_pager);
+		try {
+			if(!isDynamic)
+			{
+					Picasso.with(activity).load(models.get(position).getImage()).resize(imageView.getResources().getDisplayMetrics().widthPixels,imageView.getResources().getDisplayMetrics().heightPixels).centerCrop().into(imageView);
+					//Picasso.with(activity).load(models.get(position).getImage()).into(imageView);
+			}
+			else {
+				ViewTreeObserver vto = imageView.getViewTreeObserver();
+				vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+											 @Override
+											 public boolean onPreDraw() {
+												 try {
+													// PicassoTrustAll.getInstance(activity).load(models.get(position).getImage()).placeholder(R.drawable.placeholder).resize(imageView.getMeasuredWidth(), imageView.getMeasuredHeight()).centerCrop().into(imageView);
+													 Picasso.with(activity).load(models.get(position).getImage()).into(imageView);
+												  }
+												 catch (Exception e)
+												 {
+													 e.printStackTrace();
+												 }
+
+
+												 return true;
+											 }
+										 });
+
+			}
+		} catch (Exception e) {
+			Log.e("image Exception", e.getMessage());
+		}
+
+		container.addView(itemView);
+		if (isDynamic) {
+			if (timer == null) {
+				pageSwitcher(5);
+			}
+		}
+
+		return itemView;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		container.removeView((LinearLayout) object);
+	}
+
+	public void pageSwitcher(int seconds) {
+		timer = new Timer(); // At this line a new Thread will be created
+		timer.scheduleAtFixedRate(new RemindTask(), 0, seconds * 1000); // delay
+		// in
+		// milliseconds
+	}
+
+	public void stopTimer()
+	{
+		if(timer!=null)
+			timer.cancel();
+	}
+
+	// this is an inner class...
+	class RemindTask extends TimerTask {
+
+		@Override
+		public void run() {
+
+			// As the TimerTask run on a seprate thread from UI thread we have
+			// to call runOnUiThread to do work on UI thread.
+			activity.runOnUiThread(new Runnable() {
+				public void run() {
+					viewPager.setCurrentItem(++page % models.size());
+				}
+			});
+
+		}
+	}
+
+}
---
+++ app/src/main/java/apextechies/etodo/fragment/Privacy.java
@@ -0,0 +1,46 @@
+package apextechies.etodo.fragment;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import apextechies.etodo.R;
+import apextechies.etodo.activity.MainActivity;
+import apextechies.etodo.network.Utilz;
+
+/**
+ * @author Samuel Robert <sam@spotsoon.com>
+ * @created on 01 Aug 2017 at 7:39 PM
+ */
+
+public class Privacy extends AppCompatActivity{
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.privacy);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			Window window = Privacy.this.getWindow();
+			window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			window.setStatusBarColor(ContextCompat.getColor(Privacy.this, R.color.colorAccent));
+		}
+
+		TextView privacytext = (TextView)findViewById(R.id.privacytext);
+		TextView name = (TextView)findViewById(R.id.name);
+		privacytext.setTypeface(Utilz.font(Privacy.this,"medium"));
+		name.setTypeface(Utilz.font(Privacy.this,"bold"));
+		name.setText("Privacy");
+		findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+}
